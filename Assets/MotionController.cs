2b58c8a9f96536e590ad7eece8ab34af3e713b9c
Paracosma <sameer@paracosma.com>
2018-03-08 16:31:32 +0545

    Init
    Base

---
+++ Assets/MotionController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotionController : MonoBehaviour {
+
+    // Use this for initialization
+    Vector3 forward, right;
+    private float movespeed =3.0f;
+
+    void Start () {
+        forward = Camera.main.transform.forward;
+        forward.y = 0;
+        forward = Vector3.Normalize(forward);
+        right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.anyKey)
+        { Move(); }
+	}
+    void Move()
+    {
+        Vector3 rightMovement = right * movespeed * Time.deltaTime * Input.GetAxis("Horizontal");
+        Vector3 upMovement = forward * movespeed * Time.deltaTime * Input.GetAxis("Vertical");
+
+        Vector3 heading = Vector3.Normalize(rightMovement + upMovement);
+        transform.forward = heading;
+        transform.position += rightMovement;
+        transform.position += upMovement;
+
+    }
+}
---
+++ Assets/MouseManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseManager : MonoBehaviour {
+    public GameObject selectedObj;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+        if(Physics.Raycast(ray, out hitInfo))
+        {
+            GameObject hitObject = hitInfo.transform.root.gameObject;
+
+            SelectObject(hitObject);
+        }
+        else
+        {
+            ClearSelection();
+        }
+	}
+
+    private void ClearSelection()
+    {
+
+        selectedObj = null;
+    }
+
+    private void SelectObject(GameObject hitObject)
+    {
+        if (hitObject != null)
+        {
+            if (hitObject == selectedObj)
+            { return; }
+        
+            ClearSelection();
+        }
+        selectedObj = hitObject;
+    }
+}
